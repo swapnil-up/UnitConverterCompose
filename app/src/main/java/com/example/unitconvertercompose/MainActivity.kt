61f0b1b427f9e4ced5f1e9237ef2736bca43f413
swapnil-up <swapnilupadhyaykharel@gmail.com>
2023-11-26 16:19:37 +0545

    making the UI with jetpack compose

--- app/src/main/java/com/example/unitconvertercompose/MainActivity.kt
+++ app/src/main/java/com/example/unitconvertercompose/MainActivity.kt
@@ -1,15 +1,35 @@
 package com.example.unitconvertercompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.unitconvertercompose.ui.theme.UnitConverterComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +42,75 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Swapnil")
+                    UnitConverter()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun UnitConverter(){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Unit Converter",modifier= Modifier.padding(16.dp))
+        OutlinedTextField(value = "", onValueChange = {//what should happen
+            } )
+        Spacer(modifier = Modifier.height(16.dp))
+        Row {
+            Box{
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown,
+                        contentDescription = "Arrow Down")
+                }
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(text ={
+                        Text(text = "Millimeter")},
+                        onClick = { /*TODO*/ })
+                    DropdownMenuItem(text ={
+                        Text(text = "Meter")},
+                        onClick = { /*TODO*/ })
+                    DropdownMenuItem(text ={
+                        Text(text = "Feet")},
+                        onClick = { /*TODO*/ })
+                    DropdownMenuItem(text ={
+                        Text(text = "Inches")},
+                        onClick = { /*TODO*/ })
+                }
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Box{
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown,
+                        contentDescription = "Arrow Down")
+                }
+                DropdownMenu(expanded = true, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(text ={
+                        Text(text = "Millimeter")},
+                        onClick = { /*TODO*/ })
+                    DropdownMenuItem(text ={
+                        Text(text = "Meter")},
+                        onClick = { /*TODO*/ })
+                    DropdownMenuItem(text ={
+                        Text(text = "Feet")},
+                        onClick = { /*TODO*/ })
+                    DropdownMenuItem(text ={
+                        Text(text = "Inches")},
+                        onClick = { /*TODO*/ })
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    UnitConverterComposeTheme {
-        Greeting("Android")
-    }
+fun UnitConverterPreview(){
+    UnitConverter()
 }
