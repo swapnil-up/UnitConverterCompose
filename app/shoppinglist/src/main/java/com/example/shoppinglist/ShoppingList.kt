093a87c5271e17e9ac2673c1a63a8125b3924cb9
swapnil-up <swapnilupadhyaykharel@gmail.com>
2023-12-04 19:54:16 +0545

    Configured the alert dialog to display an add item list

--- app/shoppinglist/src/main/java/com/example/shoppinglist/ShoppingList.kt
+++ app/shoppinglist/src/main/java/com/example/shoppinglist/ShoppingList.kt
@@ -2,12 +2,15 @@ package com.example.shoppinglist
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -20,10 +23,13 @@ import androidx.compose.ui.unit.dp
 
 data class ShoppingItem(val id:Int, var name:String, var quantity:Int, val isEditing: Boolean=false)
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShoppingListApp(){
-    val shoppingItem by remember{ mutableStateOf(listOf<ShoppingItem>()) }
+    var shoppingItem by remember{ mutableStateOf(listOf<ShoppingItem>()) }
     var showDialog by remember{ mutableStateOf(false) }
+    var itemName by remember{ mutableStateOf("") }
+    var itemQuantity by remember{ mutableStateOf("") }
 
     Column(
         modifier= Modifier.fillMaxSize(),
@@ -43,8 +49,44 @@ fun ShoppingListApp(){
     }
 
     if (showDialog){
-        AlertDialog(onDismissRequest = { showDialog=false }, confirmButton = { /*TODO*/ },
-            title={Text(text = "I'M ALIVE")
+        AlertDialog(onDismissRequest = { showDialog=false },
+            confirmButton = { Row(modifier = Modifier.fillMaxSize().padding(12.dp),
+                horizontalArrangement = Arrangement.SpaceBetween) {
+                Button(onClick = {
+                    if (itemName.isNotBlank()) {
+                        val newItem= ShoppingItem(
+                            id=shoppingItem.size+1,
+                            name=itemName,
+                            quantity = itemQuantity.toInt()
+                        )
+                        shoppingItem=shoppingItem+newItem
+                        itemName=""
+                        showDialog=false
+                    }
+                    }) {
+                    Text(text = "Add")
+                }
+                Button(onClick = { showDialog=false }) {
+                    Text(text = "Cancel")
+                }
+            } },
+            title={Text(text = "Add Shopping Item")},
+            text = {
+                Column {
+                    OutlinedTextField(value = itemName,
+                        onValueChange = {itemName=it},
+                        Modifier.padding(8.dp),
+                        singleLine = true,
+                        placeholder = { Text(text = "Item Name")
+                        }
+                        )
+                    OutlinedTextField(value = itemQuantity,
+                        onValueChange = {itemQuantity=it},
+                        Modifier.padding(8.dp),
+                        singleLine=true,
+                        placeholder = { Text(text = "Item Quantity")}
+                        )
+                }
         })
     }
 }
